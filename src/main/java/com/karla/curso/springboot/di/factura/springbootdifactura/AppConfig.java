5f1af1b1c26d942def480d30fa51d25327d678d3
ksolis <limonsito.kls@hotmail.com>
2024-02-26 05:04:49 +0100

    crud en memoria

---
+++ src/main/java/com/karla/curso/springboot/di/factura/springbootdifactura/AppConfig.java
@@ -0,0 +1,22 @@
+package com.karla.curso.springboot.di.factura.springbootdifactura;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import models.Item;
+import models.Product;
+
+@Configuration
+@PropertySource(value = "classpath:data.properties", encoding = "UTF-8")
+public class AppConfig {
+    @Bean
+    List<Item> itemsInvoice() {
+        Product p1 = new Product("Camara Sony", 800);
+        Product p2 = new Product("Bicicleta Bianchi 26", 1200);
+        return Arrays.asList(new Item(p1, 2), new Item(p2, 4));
+    }
+}
---
+++ src/main/java/controllers/InvoiceController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import models.Invoice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+@RequestMapping("/invoices")
+public class InvoiceController {
+    
+    @Autowired
+    private Invoice invoice;
+
+    @GetMapping("/show")
+    public Invoice show() {
+        return invoice; 
+    }
+}
